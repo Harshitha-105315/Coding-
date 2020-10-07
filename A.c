e9253d533ed54b9643bc479574c115eaaf51a98d
harshitha <49196169+Harshitha-105315@users.noreply.github.com>
2020-10-07 11:52:50 +0530

    Create A.c

---
+++ A.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main() {
+    int choice,price=0,item;
+    int numberofplates[]={2,2,0};
+    do
+    {
+        printf("\n------menu------\n");
+            printf("1.manchuria \n2.noodles\n3.idly4.default"  );
+    printf("select choice");
+    scanf("%d",&choice);
+/*int numberofmanchuria=numberofplates[0];
+int numberofnoodles=numberofplates[1];
+
+int numberofidly=numberofplates[2];*/
+
+    switch(choice)
+    {
+        
+        case 1:
+                if(numberofplates[0]!=0){
+        	
+         price+=50;
+         printf("%d",price);
+         
+         numberofplates[0]--;
+         }
+         else
+         printf("item not availabale");
+
+                  break;
+        case 2:
+        if(numberofplates[1]!=0)
+        {
+          price+=60;
+          printf("%d",price);
+          numberofplates[1]--;
+          	}
+          	else
+          	printf("item not availabale");
+
+          break;
+         case 3:
+         if(numberofplates[2]!=0)
+         {
+          price+=40;
+          printf("%d",price);
+          numberofplates[2]--;
+          }
+          else
+          printf("item not availabale");
+
+          break;
+         default:
+          printf("invalid choice");
+         
+    }
+    }while(choice!=4);
+    printf("total bill");
+    printf("%d",price);
+    
+          
+    return 0;
+}
+                
